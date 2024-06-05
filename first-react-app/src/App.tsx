cf06af4a4e8c8449d8448adc5709a874017cd539
Tushar Verma <tusharwork.001@gmail.com>
2024-06-05 18:01:31 +0530

    First Lesson: Created a Button click for Alert with React and bootstrap

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
+import Alert from './components/Alert'
 import Button from './components/Button'
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false)
 
   return (
     <div>
-      <Button color='danger' onClick={() => console.log('Clicked')}>My Button</Button>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+      <Button color='primary' onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   )
 }
